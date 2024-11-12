24790be2134eee2bf535878bb5ca72c3483c2c2e
Beknazar <85113284+beknazar93@users.noreply.github.com>
2024-11-12 20:39:56 +0600

    Update2 Manager.js
    3

--- src/Components/Admin/Manager/Manager.js
+++ src/Components/Admin/Manager/Manager.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const Manager = () => {
-  const SHEETDB_URL = 'https://beknazarosh.pythonanywhere.com/api/clients'; // Замените на ваш URL
+  const SHEETDB_URL = 'https://beknazarosh.pythonanywhere.com/api/clients/?format=json'; // Замените на ваш URL
   const [activeTab, setActiveTab] = useState('list');
   const [clients, setClients] = useState([]);
   const [newClient, setNewClient] = useState({
@@ -24,39 +24,21 @@ const Manager = () => {
   const [selectedYear, setSelectedYear] = useState('Все годы');
   const [selectedMonth, setSelectedMonth] = useState('Все месяцы');
   const [selectedDay, setSelectedDay] = useState('Все дни');
-  const [selectedPayment, setSelectedPayment] = useState('Все оплаты');
 
-  const [items, setItems] = useState([]);
-  const [totalIncome, setTotalIncome] = useState(0); // Состояние для общего дохода
-  const [filteredIncome, setFilteredIncome] = useState(0); // Состояние для дохода по фильтрам
-
-  // Состояние для поиска
-  const [searchTerm, setSearchTerm] = useState('');
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://beknazarosh.pythonanywhere.com/api/clients');
-        setItems(response.data);
-      } catch (error) {
-        console.error('Ошибка при получении данных:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
+  // Получаем клиентов из Google Sheets
   const fetchClients = async () => {
     try {
-      const response = await axios.get('https://beknazarosh.pythonanywhere.com/api/clients');
+      const response = await axios.get(SHEETDB_URL);
       setClients(response.data);
     } catch (error) {
       console.error('Ошибка при получении клиентов:', error);
     }
+    // axios('https://beknazarosh.pythonanywhere.com/api/clients/?format=json')
+    // .then(({data})=> setClients(data)).catch(error=> console.log(error))
   };
 
   useEffect(() => {
-    fetchClients();
+    fetchClients(); // Получаем клиентов при монтировании компонента
   }, []);
 
   const handleChange = (e) => {
@@ -67,10 +49,10 @@ const Manager = () => {
   const handleAddClient = async () => {
     if (newClient.name && newClient.phone && newClient.price) {
       try {
-        await axios.post(SHEETDB_URL, {
+        await axios.post('https://beknazarosh.pythonanywhere.com/api/clients/', {
           data: newClient,
         });
-        fetchClients();
+        fetchClients(); // Обновляем список клиентов
 
         setNewClient({
           name: '',
@@ -95,39 +77,25 @@ const Manager = () => {
     }
   };
 
-  const [expandedClients, setExpandedClients] = useState({});
-
-  const handleClientClick = (index) => {
-    setExpandedClients((prev) => ({
-      ...prev,
-      [index]: !prev[index],
-    }));
-  };
-
-  const calculateIncome = (clientsList) => {
-    return clientsList.reduce((acc, client) => {
-      return acc + (parseFloat(client.price) || 0); // Суммируем цену клиента
-    }, 0);
-  };
-
-  // Фильтрация клиентов
-  const filteredClients = clients.filter((client) => {
+  // Фильтруем клиентов по выбранным категориям
+  // const filteredClients = clients.filter((client) => {
+  //   return (
+  //     (selectedSport === 'Все виды спорта' || client.sportCategory === selectedSport) &&
+  //     (selectedTrainer === 'Все тренеры' || client.trainer === selectedTrainer) &&
+  //     (selectedYear === 'Все годы' || client.year === selectedYear) &&
+  //     (selectedMonth === 'Все месяцы' || client.month === selectedMonth) &&
+  //     (selectedDay === 'Все дни' || client.day === selectedDay)
+  //   );
+  // });
+  const filteredClients = Array.isArray(clients) ? clients.filter((client) => {
     return (
       (selectedSport === 'Все виды спорта' || client.sportCategory === selectedSport) &&
       (selectedTrainer === 'Все тренеры' || client.trainer === selectedTrainer) &&
       (selectedYear === 'Все годы' || client.year === selectedYear) &&
       (selectedMonth === 'Все месяцы' || client.month === selectedMonth) &&
-      (selectedDay === 'Все дни' || client.day === selectedDay) &&
-      (selectedPayment === 'Все оплаты' || client.payment === selectedPayment) &&
-      client.name.toLowerCase().includes(searchTerm.toLowerCase()) // Поиск по имени клиента
+      (selectedDay === 'Все дни' || client.day === selectedDay)
     );
-  });
-
-  // Обновляем доходы при изменении списка клиентов или фильтров
-  useEffect(() => {
-    setTotalIncome(calculateIncome(clients)); // Общее количество дохода
-    setFilteredIncome(calculateIncome(filteredClients)); // Доход по фильтрам
-  }, [clients, filteredClients]);
+  }) : [];
 
   return (
     <div className="manager">
@@ -144,13 +112,6 @@ const Manager = () => {
         {activeTab === 'list' && (
           <div className="manager__clients-list">
             <div className="manager__filters">
-              <input
-                type="text"
-                placeholder="Поиск клиента"
-                className="manager__search-input"
-                value={searchTerm}
-                onChange={(e) => setSearchTerm(e.target.value)}
-              />
               <select
                 className="manager__filter"
                 value={selectedSport}
@@ -221,165 +182,48 @@ const Manager = () => {
                   <option key={i + 1} value={i + 1}>{i + 1}</option>
                 ))}
               </select>
-
-              <select
-                className="manager__filter"
-                value={selectedPayment}
-                onChange={(e) => setSelectedPayment(e.target.value)}
-              >
-                <option value="Все оплаты">Все оплаты</option>
-                <option value="Оплачено">Оплачено</option>
-                <option value="Не оплачено">Не оплачено</option>
-              </select>
             </div>
 
-            <div className="manager__income">
-              <p>Общий доход: {totalIncome} сом</p>
-              <p>Фильтрованный доход: {filteredIncome} сом</p>
-            </div>
-
-            <div className="manager__client-cards">
-              {filteredClients.map((client, index) => (
-                <div key={index} className="manager__client-card" onClick={() => handleClientClick(index)}>
-                  <h3 className="manager__client-name">{client.name}</h3>
-                  <p className={`manager__client-payment manager__client-payment--${client.payment === 'Оплачено' ? 'paid' : 'unpaid'}`}>
-                    Оплата: {client.payment}
-                  </p>
-                  {/* Show additional info only if the client card is expanded */}
-                  {expandedClients[index] && (
-                    <>
-                      <p className="manager__client-price">Цена: {client.price} ₽</p>
-                      <p className="manager__client-sport">Спорт: {client.sportCategory}</p>
-                      <p className="manager__client-trainer">Тренер: {client.trainer}</p>
-                      <p className="manager__client-comment">Дата: {client.day} {client.month} {client.year}</p>
-                      <p className="manager__client-comment">Комментарий: {client.comment}</p>
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
+            {filteredClients.map((client, index) => (
+              <div key={index} className="manager__client-card">
+                <h3 className="manager__client-name">{client.name}</h3>
+                <p className="manager__client-phone">Телефон: {client.phone}</p>
+                <p
+                  className={`manager__client-payment manager__client-payment--${client.payment === 'Оплачено' ? 'paid' : 'unpaid'}`}
+                >
+                  Оплата: {client.payment}
+                </p>
+                <p className="manager__client-price">Цена: {client.price} ₽</p>
+                <p className="manager__client-sport">Спорт: {client.sportCategory}</p>
+                <p className="manager__client-trainer">Тренер: {client.trainer}</p>
+                <p className="manager__client-comment">Комментарий: {client.comment}</p>
+                <p className="manager__client-date">Дата: {client.day} {client.month} {client.year}</p>
+              </div>
+            ))}
           </div>
         )}
-
-        {/* {activeTab === 'add' && (
-          <div className="manager__add-client">
-            <h2>Добавить клиента</h2>
-            <input
-              type="text"
-              name="name"
-              placeholder="Имя клиента"
-              value={newClient.name}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="phone"
-              placeholder="Телефон клиента"
-              value={newClient.phone}
-              onChange={handleChange}
-            />
-            <input
-              type="text"
-              name="price"
-              placeholder="Цена"
-              value={newClient.price}
-              onChange={handleChange}
-            />
-            <select
-              name="sportCategory"
-              value={newClient.sportCategory}
-              onChange={handleChange}
-            >
-              <option value="Бокс">Бокс</option>
-              <option value="ММА">ММА</option>
-              <option value="Дзюдо">Дзюдо</option>
-              <option value="Самбо">Самбо</option>
-              <option value="Борьба">Борьба</option>
-              <option value="Тхэквандо">Тхэквандо</option>
-              <option value="Греко-римская борьба">Греко-римская борьба</option>
-            </select>
-            <select
-              name="trainer"
-              value={newClient.trainer}
-              onChange={handleChange}
-            >
-              <option value="Надиров Арап">Надиров Арап</option>
-              <option value="Минбаев Сулейман">Минбаев Сулейман</option>
-              <option value="Саттаров Канатбек">Саттаров Канатбек</option>
-              <option value="Абдуманап Уулу Илим">Абдуманап Уулу Илим</option>
-              <option value="Онарбоев Акжол">Онарбоев Акжол</option>
-              <option value="Айдар Уулу Эржигит">Айдар Уулу Эржигит</option>
-            </select>
-            <select
-              name="payment"
-              value={newClient.payment}
-              onChange={handleChange}
-            >
-              <option value="Оплачено">Оплачено</option>
-              <option value="Не оплачено">Не оплачено</option>
-            </select>
-            <textarea
-              name="comment"
-              placeholder="Комментарий"
-              value={newClient.comment}
-              onChange={handleChange}
-            />
-            <select name="year" value={newClient.year} onChange={handleChange}>
-              <option value="Все годы">Все годы</option>
-              <option value="2022">2022</option>
-              <option value="2023">2023</option>
-              <option value="2024">2024</option>
-            </select>
-            <select name="month" value={newClient.month} onChange={handleChange}>
-              <option value="Все месяцы">Все месяцы</option>
-              <option value="Январь">Январь</option>
-              <option value="Февраль">Февраль</option>
-              <option value="Март">Март</option>
-              <option value="Апрель">Апрель</option>
-              <option value="Май">Май</option>
-              <option value="Июнь">Июнь</option>
-              <option value="Июль">Июль</option>
-              <option value="Август">Август</option>
-              <option value="Сентябрь">Сентябрь</option>
-              <option value="Октябрь">Октябрь</option>
-              <option value="Ноябрь">Ноябрь</option>
-              <option value="Декабрь">Декабрь</option>
-            </select>
-            <select name="day" value={newClient.day} onChange={handleChange}>
-              <option value="Все дни">Все дни</option>
-              {Array.from({ length: 31 }, (_, i) => (
-                <option key={i + 1} value={i + 1}>{i + 1}</option>
-              ))}
-            </select>
-
-            <button className="manager__add-button" onClick={handleAddClient}>
-              {addButtonText}
-            </button>
-          </div>
-        )} */}
 
         {activeTab === 'add' && (
           <div className="manager__add-client">
-            <h2 className="manager__add-client-title">Добавить клиента</h2>
             <input
-              className="manager__add-client-input"
               type="text"
               name="name"
-              placeholder="Имя клиента"
+              placeholder="Имя"
+              className="manager__input"
               value={newClient.name}
               onChange={handleChange}
             />
             <input
-              className="manager__add-client-input"
               type="text"
               name="phone"
-              placeholder="Телефон клиента"
+              placeholder="Телефон"
+              className="manager__input"
               value={newClient.phone}
               onChange={handleChange}
             />
             <select
-              className="manager__add-client-select"
               name="sportCategory"
+              className="manager__select"
               value={newClient.sportCategory}
               onChange={handleChange}
             >
@@ -392,8 +236,8 @@ const Manager = () => {
               <option value="Греко-римская борьба">Греко-римская борьба</option>
             </select>
             <select
-              className="manager__add-client-select"
               name="trainer"
+              className="manager__select"
               value={newClient.trainer}
               onChange={handleChange}
             >
@@ -404,23 +248,13 @@ const Manager = () => {
               <option value="Онарбоев Акжол">Онарбоев Акжол</option>
               <option value="Айдар Уулу Эржигит">Айдар Уулу Эржигит</option>
             </select>
-            <select
-              className="manager__add-client-select"
-              name="year"
-              value={newClient.year}
-              onChange={handleChange}
-            >
+            <select name="year" className="manager__select" value={newClient.year} onChange={handleChange}>
               <option value="Все годы">Все годы</option>
               <option value="2022">2022</option>
               <option value="2023">2023</option>
               <option value="2024">2024</option>
             </select>
-            <select
-              className="manager__add-client-select"
-              name="month"
-              value={newClient.month}
-              onChange={handleChange}
-            >
+            <select name="month" className="manager__select" value={newClient.month} onChange={handleChange}>
               <option value="Все месяцы">Все месяцы</option>
               <option value="Январь">Январь</option>
               <option value="Февраль">Февраль</option>
@@ -435,28 +269,23 @@ const Manager = () => {
               <option value="Ноябрь">Ноябрь</option>
               <option value="Декабрь">Декабрь</option>
             </select>
-            <select
-              className="manager__add-client-select"
-              name="day"
-              value={newClient.day}
-              onChange={handleChange}
-            >
+            <select name="day" className="manager__select" value={newClient.day} onChange={handleChange}>
               <option value="Все дни">Все дни</option>
               {Array.from({ length: 31 }, (_, i) => (
                 <option key={i + 1} value={i + 1}>{i + 1}</option>
               ))}
             </select>
             <input
-              className="manager__add-client-input"
-              type="text"
+              type="number"
               name="price"
               placeholder="Цена"
+              className="manager__input"
               value={newClient.price}
               onChange={handleChange}
             />
             <select
-              className="manager__add-client-select"
               name="payment"
+              className="manager__select"
               value={newClient.payment}
               onChange={handleChange}
             >
@@ -464,20 +293,19 @@ const Manager = () => {
               <option value="Не оплачено">Не оплачено</option>
             </select>
             <textarea
-              className="manager__add-client-textarea"
               name="comment"
               placeholder="Комментарий"
+              className="manager__textarea"
               value={newClient.comment}
               onChange={handleChange}
             />
-            <button className="manager__add-client-button" onClick={handleAddClient}>
-              {addButtonText}
-            </button>
+            <button className="manager__add-button" onClick={handleAddClient}>{addButtonText}</button>
           </div>
         )}
-
       </div>
     </div>
   );
 };
+
 export default Manager;
+
